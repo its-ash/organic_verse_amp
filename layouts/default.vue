094f8c6a6399622c1336c0129336f7b2a4ac4abd
Ashvini Jangid <ashvinijangid@gmail.com>
2022-06-05 02:59:02 +0530

    Header Complete

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/Header";
+export default {
+  name: "default",
+  components:{Header}
+};
+</script>
+
+<style lang="scss">
+@import "assets/style.scss";
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,32 @@
 import { firebaseConfig } from './firebase'
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'organic_verse_amp',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        content: 'width=device-width, initial-scale=1'
       },
       { hid: 'description', name: 'description', content: '' },
       {
         name: 'format-detection',
-        content: 'telephone=no',
-      },
+        content: 'telephone=no'
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/amp', '@nuxtjs/firebase'],
+  modules: ['@nuxtjs/firebase'],
   amp: {
     // Options
   },
@@ -54,13 +39,11 @@ export default {
       storageBucket: firebaseConfig.storageBucket,
       messagingSenderId: firebaseConfig.messagingSenderId,
       appId: firebaseConfig.appId,
-      measurementId: firebaseConfig.measurementId,
+      measurementId: firebaseConfig.measurementId
     },
     services: {
-      firestore: true,
-    },
+      firestore: true
+    }
   },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {}
 }
